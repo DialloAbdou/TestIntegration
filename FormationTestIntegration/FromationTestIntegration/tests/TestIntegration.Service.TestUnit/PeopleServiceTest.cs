2aa9bf2e2caf38cbfb8fb951fddd45a6b2719211
DialloAbdou <abdoudiallo3826@yahoo.fr>
2024-03-04 15:38:22 +0100

    structuré le projet

---
+++ FormationTestIntegration/FromationTestIntegration/tests/TestIntegration.Service.TestUnit/PeopleServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestIntegration.Data.Abstractions;
+using TestIntegration.Service.Abstractions;
+
+namespace TestIntegration.Service.TestUnit
+{
+    public class PeopleServiceTest
+    {
+        private Mock<IPersonneRepository> _personneRepositoryMock = new Mock<IPersonneRepository>();
+        private Mock<ITextOutput> _textOutputMock = new Mock<ITextOutput>();
+
+        public async Task PeopleService_Sholud_Use_PersonneIdentityFormater()
+        {
+        
+        }
+
+    }
+}
