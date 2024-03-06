fc72ffdd2fc92880e2ad52b07503651a4b8b56df
DialloAbdou <abdoudiallo3826@yahoo.fr>
2024-03-06 21:02:54 +0100

    test integration sur les fichier

---
+++ FormationTestIntegration/FromationTestIntegration/src/TestIntegration.Data/Implementations/FilePersonneRepository.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using TestIntegration.Data.Abstractions;
+using TestIntegration.Models;
+using static System.Environment;
+
+namespace TestIntegration.Data.Implementations
+{
+    public class FilePersonneRepository : IPersonneRepository
+    {
+        public FilePersonneRepository() 
+        {
+            if (!File.Exists("personne.csv"))
+            {
+                File.WriteAllText($"personne.csv", $"Prenom; Nom;BirthDay {NewLine}");
+            }
+        }
+        public async Task AddPersonne(Personne personne)
+        {
+            var listePeoples = await GetAllPersonnes();
+            if(!listePeoples.All(p=>p.Prenom == personne.Prenom && p.Nom ==  personne.Nom))
+            {
+               await  File.AppendAllTextAsync($"personne.csv", $"{personne.Nom};{personne.Prenom};{personne!.BirthDay:dd/mm/yyyyy}{NewLine}");
+            }
+        }
+
+        public async Task<IEnumerable<Personne>> GetAllPersonnes()
+        {
+            IEnumerable<Personne> listpersonne = new List<Personne>();
+           
+            foreach (var item in( await File.ReadAllLinesAsync("personne.csv")).Skip(1))
+            {
+                var data = item.Split(';');
+                 listpersonne.Append( new Personne
+                {
+                    Nom = data[0],
+                    Prenom = data[1],
+                    BirthDay = DateTime.ParseExact(data[2], "dd/mm/yyyy", null)
+                });
+            }
+            return listpersonne;
+        }
+    }
+}
---
+++ FormationTestIntegration/FromationTestIntegration/tests/TestIntegration.Data.testsUnits/Implimentations/FileRepositoryTests.cs
@@ -0,0 +1,59 @@
+﻿using FluentAssertions;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using TestIntegration.Data.Implementations;
+
+namespace TestIntegration.Data.testsUnits.Implimentations
+{
+    public class FileRepositoryTests : IDisposable
+    {
+
+
+        public FileRepositoryTests()
+        {
+            if(File.Exists("personne.csv"))
+            {
+                File.Delete("personne.csv");
+            }
+        }
+
+
+        [Fact]
+        public void  Ctor_Should_Create_File_Not_Existed()
+        {
+          
+                File.Exists("personne.csv").Should().BeFalse();
+
+                var repo = new FilePersonneRepository();
+
+                File.Exists("personne.csv").Should().BeTrue();
+          
+
+        }
+
+
+
+
+        [Fact]
+        public async Task Add_Should_Weite_Personne_In_File_()
+        {
+
+                var repo = new FilePersonneRepository();
+                await repo.AddPersonne(new Models.Personne
+                {
+                    Nom = "Diallo",
+                    Prenom = "Abdou",
+                    BirthDay = new DateTime(1980, 5, 6)
+
+                });
+
+            var list = await repo.GetAllPersonnes();
+
+            list.Should().NotBeNull();
+        }
+
+        public void Dispose()
+        {
+            File.Delete("personne.csv");
+        }
+    }
+}
